c42712ff2cc08e11eeebf1e0e74c44df14f630a7
Minal <minal.shende@invenso.nl>
2024-08-16 13:36:34 +0200

    first commit

---
+++ src/store/slices/authSlice.ts
@@ -0,0 +1,43 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../appStore";
+
+export interface User {
+  email: string | null;
+  uid?: string;
+  displayName?: any;
+  phonenumber?: number;
+  password?: string;
+}
+
+interface AuthState {
+  isAuthenticated?: boolean;
+  isLoading?: boolean;
+  user?: User;
+}
+const initialState: AuthState = {
+  isAuthenticated: false,
+  isLoading: false,
+  user: undefined,
+};
+export const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    setLoading(state, action: PayloadAction<boolean>) {
+      state.isLoading = action.payload;
+    },
+    setUser(state, action: PayloadAction<AuthState["user"]>) {
+      const isAuthenticated = true;
+      state.user = action.payload;
+      state.isAuthenticated = isAuthenticated;
+    },
+    setAuthUser(state, action: PayloadAction<any>) {},
+    signOut(state) {},
+  },
+});
+
+export const userReducer = authSlice.reducer;
+
+export const { setUser, setLoading, signOut } = authSlice.actions;
+
+export const authUser = (state: RootState) => state.auth.user;
---
+++ src/store/slices/cartSlice.ts
@@ -0,0 +1,45 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { RootState } from "../appStore";
+
+export interface CartItem {
+  id: number;
+  name: string;
+  price: number;
+}
+
+interface CartState {
+  items: CartItem[];
+}
+const initialState: CartState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action: PayloadAction<CartItem>) => {
+      // mutating the state here
+      // redux uses immer library behind the scene
+      // immer takes new state and old state then calculate difference and updates state
+      state.items.push(action.payload);
+    },
+    removeItem: (state, action: PayloadAction<CartItem>) => {
+      state.items.pop();
+    },
+    // originalState = ["pizza"]
+    clearCart: (state) => {
+      // RTK -either mutate the existing state or return a new state
+      //state.items.length = 0; //originalstate = [];
+      state.items.length = 0; // or you can also write "return {items: []}"
+      // this new object will be replaced inside originalstate = []
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+
+export const cartReducer = cartSlice.reducer;
+
+//subscribing to store using selector
+export const cartItemsSelector = (state: RootState) => state.cart.items;
